cd904c18a0550ca3df45d2a65a4a096d1722c42d
Dionmax F. Nobre <dionmaxfn@gmail.com>
2023-10-22 15:32:27 -0300

    feat: updating api

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,20 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Form\MovieType;
 use App\Repository\MovieRepository;
+use Doctrine\DBAL\Exception;
+use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Type;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-/**
- * @Route("/movies")
- */
+#[Route('/movies')]
 class MovieController extends AbstractController
 {
-    /**
-     * @Route("")
-     */
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/', methods: 'GET')]
     public function getMovies(MovieRepository $movieRepository)
     {
         $data = $movieRepository->findAll();
@@ -29,9 +37,97 @@ class MovieController extends AbstractController
         );
     }
 
+    #[Route('/{id}', methods: 'GET')]
+    public function getMovie(MovieRepository $movieRepository, int $id)
+    {
+        $data = $movieRepository->findMovie($id);
+
+        if (array_key_exists(0, $data))
+            return new JsonResponse(
+                [
+                    $data[0]
+                ],
+                Response::HTTP_OK,
+            );
+        else
+            return new JsonResponse(
+                [],
+                Response::HTTP_NOT_FOUND,
+            );
+    }
+
+    #[Route('', methods: 'POST')]
+    public function addMovie(MovieRepository $movieRepository, Request $request)
+    {
+
+        $movie = new Movie();
+
+        $form = $this->createForm(MovieType::class, $movie);
+
+        $form->submit($request->toArray());
+
+        $this->entityManager->persist($movie);
+
+        $this->entityManager->flush();
+
+        return new JsonResponse(
+            [],
+            Response::HTTP_OK,
+        );
+    }
+
+    #[Route('/{id}', methods: 'PUT')]
+    public function updateMovie(MovieRepository $movieRepository, int $id, Request $request)
+    {
+        $movie = $movieRepository->find($id);
+
+        if (is_null($movie)) {
+            return new JsonResponse(
+                [],
+                Response::HTTP_NOT_FOUND,
+            );
+        }
+
+        $form = $this->createForm(MovieType::class, $movie, array('method' => 'PUT'));
+
+        $form->submit($request->toArray());
+
+        $this->entityManager->persist($movie);
+
+        $this->entityManager->flush();
+
+        return new JsonResponse(
+            [],
+            Response::HTTP_OK,
+        );
+
+    }
+
     /**
-     * @Route("/resetdatabase")
+     * @throws Exception
      */
+    #[Route('/{id}', methods: 'DELETE')]
+    public function deleteMovie(MovieRepository $movieRepository, int $id)
+    {
+        $data = $movieRepository->findMovie($id);
+
+        if (array_key_exists(0, $data)) {
+            $movieRepository->deleteMovie($data[0]['id']);
+
+            return new JsonResponse(
+                [],
+                Response::HTTP_NO_CONTENT,
+            );
+        } else {
+            return new JsonResponse(
+                [],
+                Response::HTTP_NOT_FOUND,
+            );
+        }
+    }
+
+    //For debug only
+    #[Route('/resetdatabase')]
     public function resetDatabase(MovieRepository $movieRepository)
     {
 
@@ -64,10 +160,9 @@ class MovieController extends AbstractController
         );
     }
 
-    /**
-     * @Route("/phpinfoextensions")
-     */
-    public function phpinfoExtensions(MovieRepository $movieRepository)
+    //For debug only
+    #[Route('/phpinfoextensions')]
+    public function phpinfoExtensions(MovieRepository $movieRepository): JsonResponse
     {
         return new JsonResponse(
             [
--- src/Repository/MovieRepository.php
+++ src/Repository/MovieRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Movie;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\DBAL\Exception;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -20,14 +21,14 @@ class MovieRepository extends ServiceEntityRepository
         parent::__construct($registry, Movie::class);
     }
 
-    public function insertData($data)
+    public function insertData($data): void
     {
 
         $em = $this->getEntityManager();
 
         $batchSize = 20;
         for ($i = 1; $i < sizeof($data); ++$i) {
-            $movie = New Movie();
+            $movie = new Movie();
             $movie->setYear($data[$i][0]);
             $movie->setTitle($data[$i][1]);
             $movie->setStudios(array_key_exists(2, $data[$i]) ? $data[$i][2] : '');
@@ -46,7 +47,10 @@ class MovieRepository extends ServiceEntityRepository
         $em->clear();
     }
 
-    public function deleteData()
+    /**
+     * @throws Exception
+     */
+    public function deleteData(): void
     {
         $sql = <<<SQL
                 delete from movie where true;
@@ -56,19 +60,58 @@ class MovieRepository extends ServiceEntityRepository
         $stmt->executeQuery();
     }
 
-    public function deleteAndInsertData($data)
+    public function deleteMovie($id): void
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+
+        $qb->delete('App:Movie', 'm')
+            ->where($qb->expr()->eq('m.id', ':id'))
+            ->setParameter(':id', $id)
+            ->getQuery()
+            ->execute();
+    }
+
+    public function updateMovie($id): void
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+
+        $qb->update('App:Movie', 'm')
+            ->where($qb->expr()->eq('m.id', ':id'));
+
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function deleteAndInsertData($data): void
     {
         $this->deleteData();
         $this->insertData($data);
     }
 
-    public function findAll()
+    /**
+     * @throws Exception
+     */
+    public function findAll(): array
     {
         $sql = <<<SQL
-                select * from movie;
+                select * from movie order by id desc;
                 SQL;
 
         $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
         return $stmt->executeQuery()->fetchAllAssociative();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function findMovie($id, $lockMode = null, $lockVersion = null): array|null
+    {
+        $sql = <<<SQL
+                select * from movie where id == :id;
+                SQL;
+
+        $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
+        return $stmt->executeQuery([':id' => $id])->fetchAllAssociative();
     }
 }
